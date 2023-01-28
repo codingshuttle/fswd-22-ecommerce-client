632ef0bf4550ed13b018a4f2af709586e3f1aaea
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-01-28 21:46:49 +0530

    update browser router to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
@@ -10,12 +10,12 @@ import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
                     <App />
                 </PersistGate>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
